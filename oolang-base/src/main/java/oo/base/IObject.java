1e61e6a6c14bc4cc33a80b6fceefbec7ead931d2
pull-vert <frederic.montariol@gmail.com>
2019-11-12 22:27:08 +0100

    add Array class

--- oolang-base/src/main/java/oo/base/IObject.java
+++ oolang-base/src/main/java/oo/base/IObject.java
@@ -14,8 +14,7 @@ import oo.base.lang.String;
 
 /**
  * The root of the Oolang interface hierarchy. Every Oolang interface has {@link IObject} as a superinterface.
- * <br>
- * {@link IObject} is virtual, it is not present in any Oolang interface corresponding .class file
+ * <p>{@link IObject} is virtual, it is not present in any Oolang interface corresponding .class file
  */
 public interface IObject {
 
---
+++ oolang-base/src/main/java/oo/base/lang/Array.java
@@ -0,0 +1,13 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+package oo.base.lang;
+
+/**
+ * Represents a Java array.
+ * <p>{@link Array} is virtual, it is not present in any Oolang corresponding .class file, but replaced by an Array
+ */
+public abstract class Array<T> extends Object {
+
+}
