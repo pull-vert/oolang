aaa264888103f0ca78f4552a61f9dac0c07761f1
pull-vert <frederic.montariol@gmail.com>
2019-11-12 18:09:22 +0100

    separate oolang-core and oolang-stdlib

--- oolang-core/src/main/java/module-info.java
+++ oolang-core/src/main/java/module-info.java
@@ -2,8 +2,10 @@
  * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
  */
 
-module oolang.stdlib {
+module oolang.core {
 	requires jsr305;
+	requires oolang.stdlib;
 
-	exports oo.internal;
+	exports oo.io;
+	exports oo.lang;
 }
