2f778ed85421393a4b95d0d69641d7f7997451bf
pull-vert <5107035+pull-vert@users.noreply.github.com>
2026-05-06 09:13:29 +0200

    Oolang compiler module WIP

---
+++ compiler/src/main/java/oolang/compiler/OolangCompiler.java
@@ -0,0 +1,24 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+package oolang.compiler;
+
+import oolang.ast.FileAst;
+import org.jspecify.annotations.NonNull;
+
+import java.nio.file.Path;
+import java.util.Objects;
+
+public final class OolangCompiler {
+    // un-instantiable
+    private OolangCompiler() {
+    }
+
+    public static void compile(final @NonNull FileAst ast, final @NonNull Path classPath) {
+        Objects.requireNonNull(ast);
+        Objects.requireNonNull(classPath);
+
+
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,9 @@ pluginManagement {
 rootProject.name = "oolang"
 
 include(":oolang-ast")
+include(":oolang-compiler")
 include(":oolang-parser")
 
 project(":oolang-ast").projectDir = file("ast")
+project(":oolang-compiler").projectDir = file("compiler")
 project(":oolang-parser").projectDir = file("parser")
